ecd8cbddce45a30dc337bf50fc8f1541577b23d9
Tristan Smith <darcnite3000@gmail.com>
2018-03-08 19:15:24 +1100

    include babel register and preset-env

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const emails = require('./lib/emails')
+require('babel-register')
+const {
+  emails
+} = require('./lib/emails')
 const fs = require('fs')
-const { argv } = require('yargs')
+const {
+  argv
+} = require('yargs')
 
-const config = require(argv.config || './src/config')
-
+const {
+  config
+} = require(argv.config || './src/config')
 console.log('email files created')
-emails(config).forEach(({ name, path, email }) => {
+emails(config).forEach(({
+  name,
+  path,
+  email
+}) => {
   console.log(path)
   fs.writeFile(`dist/${name}`, email, 'utf8', err => {
     if (err) return console.log(err)
